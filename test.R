53e76cff4b1cb22f57277ca48fa4575f777e8e80
Karthik Ram <karthik.ram@gmail.com>
2015-08-21 11:20:46 +0200

    Added some error handling

--- test.R
+++ test.R
@@ -6,7 +6,10 @@ fake_people <- function(num_results = 20,
                         nat = 'US') {
 # Function will still need some error checking
 # e.g. a country that's not valid should be removed
-  
+if(num_results > 1000) {
+	stop("What the hell are you thinking?")
+}
+
 url <- "http://api.randomuser.me/"
 args <- list(gender = gender_wanted,
              results = num_results,
